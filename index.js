4f33f82e36edaf9b43eeb2bbaf43cd1d63116e28
Chris <chris.posthumus@gmail.com>
2023-01-11 16:08:06 +0200

    Update index.js

--- index.js
+++ index.js
@@ -159,6 +159,7 @@ class TuyaPlatformGeyserwise {
         this.deviceAccessories.set(uuid, deviceAccessory);
         break;
       case 'qn':
+        case 'rs': //Geyserwise MAX
         deviceAccessory = new HeaterAccessory(this, homebridgeAccessory, device);
         this.accessories.set(uuid, deviceAccessory.homebridgeAccessory);
         this.deviceAccessories.set(uuid, deviceAccessory);
@@ -184,11 +185,11 @@ class TuyaPlatformGeyserwise {
         this.accessories.set(uuid, deviceAccessory.homebridgeAccessory);
         this.deviceAccessories.set(uuid, deviceAccessory);
         break;
-      case 'rs': //Geyserwise MAX
+      /*case 'rs': //Geyserwise MAX
         deviceAccessory = new GeyserAccessory(this, homebridgeAccessory, device);
         this.accessories.set(uuid, deviceAccessory.homebridgeAccessory);
         this.deviceAccessories.set(uuid, deviceAccessory);
-        break;
+        break;*/
       default:
         break;
     }
